86610d505f6b8c19cdba00ca61d52d5187e6aff9
Mael <rgdmael@gmail.com>
2021-11-15 09:07:56 +0100

    init project

---
+++ C2H5OH/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace C2H5OH
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
