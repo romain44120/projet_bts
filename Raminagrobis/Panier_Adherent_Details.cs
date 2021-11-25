ca7c2ed9ca648a0e2a10498611c45e7b6c2dfbfa
roro44 <romain.dillet44@gmail.com>
2021-11-25 10:29:08 +0100

    Add class

---
+++ Raminagrobis/Panier_Adherent_Details.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Raminagrobis
+{
+
+    class Panier_Adherent_Details
+    {
+        //id reference
+
+        private string quantite;
+        public string QUANTITE
+        {
+            get { return quantite; }
+            private set { quantite = value; }
+        }
+        
+        //id panier adherent
+
+ 
+        public int ID { get; private set; }
+
+        public Panier_Adherent_Details(string quantite)
+        {
+            QUANTITE = quantite;
+
+        }
+        public Panier_Adherent_Details(int id, string quantite)
+            : this(quantite)
+        {
+            ID = id;
+        }
+
+    }
+}
