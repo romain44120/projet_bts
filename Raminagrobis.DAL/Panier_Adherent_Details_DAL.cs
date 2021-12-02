d32fc0a70f8fb1c0e38578bb0f8be851c78af064
roro44 <romain.dillet44@gmail.com>
2021-12-02 11:18:56 +0100

    feat : add all DAL

---
+++ Raminagrobis.DAL/Panier_Adherent_Details_DAL.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Raminagrobis.DAL
+{
+    public class Panier_Adherent_Details_DAL
+    {
+        public int ID { get; set; }
+        public string QUANTITE { get; set; }
+        public int ID_REFERENCE { get; set; }
+        public int ID_PANIER_ADHERENT { get; set; }
+
+        public Panier_Adherent_Details_DAL(string quantite, int id_reference, int id_panier_adherent)
+            => (QUANTITE, ID_REFERENCE, ID_PANIER_ADHERENT) = (quantite, id_reference, id_panier_adherent);
+
+        public Panier_Adherent_Details_DAL(int id, string quantite, int id_reference, int id_panier_adherent)
+                    => (ID, QUANTITE, ID_REFERENCE, ID_PANIER_ADHERENT) = (id, quantite, id_reference, id_panier_adherent);
+
+        
+
+    }
+}
---
+++ Raminagrobis.DAL/Panier_Globals_DAL.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Raminagrobis.DAL
+{
+    public class Panier_Global
+    {
+        public int ID { get; set; }
+        public string SEMAINE { get; set; }
+
+
+        public Panier_Global(string semaine)
+            => (SEMAINE) = (semaine);
+
+
+        public Panier_Global(int id, string semaine)
+                    => (ID, SEMAINE) = (id, semaine);
+
+        
+
+    }
+}
